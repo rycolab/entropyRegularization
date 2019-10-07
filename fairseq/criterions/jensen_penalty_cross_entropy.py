37e1d77a0e71e7fb0cfbf55109edc403d50abba9
Clara Meister <cmeister7.47@gmail.com>
2019-10-07 16:52:00 +0200

    Adding unigram dist as base

--- fairseq/criterions/jensen_penalty_cross_entropy.py
+++ fairseq/criterions/jensen_penalty_cross_entropy.py
@@ -6,15 +6,14 @@ from fairseq import utils
 from . import FairseqCriterion, register_criterion
 import torch.distributions as d
 
-def smoothed_nll_loss(lprobs, target, alpha, beta, ignore_index=None, reduce=True):
+def smoothed_nll_loss(lprobs, target, alpha, beta, dist, ignore_index=None, reduce=True):
     if target.dim() == lprobs.dim() - 1:
         target = target.unsqueeze(-1)
     nll_loss = -lprobs.gather(dim=-1, index=target)
     ent_p = -torch.exp(lprobs).mul(lprobs).sum(dim=-1, keepdim=True)
-    uni_probs = torch.ones(lprobs.size(), device=lprobs.device) * 1./lprobs.size(-1)
-    # not including uniform dist entropy since its constant
-    #ent_u = -torch.log(uni_probs).mul(uni_probs).sum(dim=-1, keepdim=True)
-    #assert ent_u.sum() >= ent_p.sum()
+    uni_probs = torch.ones(lprobs.size(), device=lprobs.device) * dist
+    print(uni_probs.sum(dim=1).isclose(torch.ones(lprobs.size(0), device=lprobs.device)))
+    assert uni_probs.sum(dim=1).isclose(torch.ones(lprobs.size(0), device=lprobs.device)).all()
 
     comb = torch.exp(lprobs) * alpha + uni_probs * (1. - alpha)
     ent_c = -torch.log(comb).mul(comb).sum(dim=-1, keepdim=True)
@@ -41,11 +40,20 @@ class JensonCrossEntropyCriterion(FairseqCriterion):
         super().__init__(args, task)
         self.alpha = args.alpha
         self.beta = args.beta
+        tgt_dict = task.target_dictionary
+        self.unigram_dist = torch.Tensor(tgt_dict.count)
+        self.unigram_dist[tgt_dict.eos_index] = self.unigram_dist[tgt_dict.index('.')]
+        self.unigram_dist = self.unigram_dist.pow(1./args.T)
+        self.unigram_dist = self.unigram_dist/self.unigram_dist.sum()
+        if torch.cuda.is_available() and not args.cpu:
+            self.unigram_dist = self.unigram_dist.cuda()
 
     @staticmethod
     def add_args(parser):
         """Add criterion-specific arguments to the parser."""
         # fmt: off
+        parser.add_argument('--T', default=1., type=float, metavar='N',
+                            help='unigram distribution annealing factor')
         parser.add_argument('--beta', default=0., type=float, metavar='D',
                             help='weight of penalty')
         parser.add_argument('--alpha', default=0.5, type=float, metavar='D',
@@ -77,7 +85,7 @@ class JensonCrossEntropyCriterion(FairseqCriterion):
         lprobs = lprobs.view(-1, lprobs.size(-1))
         target = model.get_targets(sample, net_output).view(-1, 1)
         loss, nll_loss = smoothed_nll_loss(
-            lprobs, target, self.alpha, self.beta, ignore_index=self.padding_idx, reduce=reduce,
+            lprobs, target, self.alpha, self.beta, self.unigram_dist, ignore_index=self.padding_idx, reduce=reduce,
         )
         return loss, nll_loss
 
